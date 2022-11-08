cde221b689b648cf4d3d8b9e607436cfa46364ec
israeloriente <israelnunesoriente@gmail.com>
2022-11-08 20:20:18 +0000

    feat: server and client with less delay

--- minitalk/server.c
+++ minitalk/server.c
@@ -3,53 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: israeloriente <israeloriente@student.42    +#+  +:+       +#+        */
+/*   By: inunes-o <inunes-o@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 12:16:51 by israelorien       #+#    #+#             */
-/*   Updated: 2022/10/30 17:32:11 by israelorien      ###   ########.fr       */
+/*   Updated: 2022/11/08 20:03:16 by inunes-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// int	ft_isprint(int arg)
-// {
-// 	return (arg >= 32 && arg < 127);
-// }
-
-static void	receive_data(int signal, siginfo_t *data, void *nao_sei)
+static void	receive_data(int signal)
 {
-	int	pid;
+	static char	cha;
+	static int	i;
 
-	pid = data->si_pid;
-	printf("%d\n", signal);
-	printf("%d\n", pid);
-	// static char	cha;
-	// static int	i;
-	// if (i < 8)
-	// {
-	// 	cha = ((signal == SIGUSR1) << i) | cha;
-	// 	i++;
-	// }
-	// if (i == 8)
-	// {
-	// 	write(1, &cha, 1);
-	// 	i = 0;
-	// 	cha = 0;
-	// }
+	if (i < 8)
+	{
+		cha = ((signal == SIGUSR1) << i) | cha;
+		i++;
+	}
+	if (i == 8)
+	{
+		write(1, &cha, 1);
+		i = 0;
+		cha = 0;
+	}
+	usleep(25);
 }
 
 int	main(void)
 {
-	struct sigaction	sig_action;
-
 	printf("%d\n", getpid());
-	sig_action.sa_sigaction = receive_data;
-	sigaction(SIGUSR1, &sig_action, NULL);
-	sigaction(SIGUSR2, &sig_action, NULL);
-	// signal(SIGUSR1, &receive_data);
-	// signal(SIGUSR2, &receive_data);
+	signal(SIGUSR1, &receive_data);
+	signal(SIGUSR2, &receive_data);
 	while (1)
 		pause();
-	return (0);
 }
